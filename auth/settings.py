7b4719255c65d41fd5f5307a6f55e378df32a42b
jefry <jefrydep@gmail.com>
2023-04-01 18:16:36 -0500

    fixing contactos urls

--- auth/settings.py
+++ auth/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +128,12 @@ STATIC_URL= '/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER='jefrydep@gmail.com'
+EMAIL_HOST_PASSWORD='iamthebestdeveloper'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
--- contactos/views.py
+++ contactos/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .forms import FormularioContacto
-
+from django.core.mail import EmailMessage
 
 
 # Create your views here.
 
 def contactos(request):
+    
     # productos=Producto.objects.all()
-    formulario_contactos=FormularioContacto()
-    return render(request, 'contactos.html',{'mi_formulario':formulario_contactos})
+    formulario_contactos = FormularioContacto()
+    if request.method == 'POST':
+        formulario_contactos=FormularioContacto(data=request.POST)
+        if formulario_contactos.is_valid():
+            nombre=request.POST.get('nombre')
+            email=request.POST.get('email')
+            contenido=request.POST.get('contenido')
+            email=EmailMessage('Mensaje ,Mensaje de : {}email addres:{}message:\n\n{}'.format(nombre,email,contenido)," ",['jefrydep@gmail.com'],reply_to=[email])
+            try:
+                email.send()
+                return redirect('contactos/?valido')
+            except:
+                return redirect('contactos/?novalido')
+
+
+    return render(request, 'contactos.html', {'mi_formulario': formulario_contactos})
