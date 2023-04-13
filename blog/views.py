3fcd5f751cd38a00266ae8a9eb84ae32e50603da
jefry <jefrydep@gmail.com>
2023-04-13 15:04:21 -0500

    blog view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Post,Categoria
 
 # Create your views here.
 def blog(request):
-    return render(request,'blog.html')
+    posts=Post.objects.all() 
+    return render(request,'blog.html',{'posts':posts})
+
+# def categoria()
